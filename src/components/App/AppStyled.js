f78f8a25e4a8e893746a3025a89aa0187aa66603
QuentinCharlie <quentin.charlie@orange.fr>
2020-06-09 17:16:50 +0200

    init

---
+++ src/components/App/AppStyled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AppStyled = styled.div`
+
+  width: 500px;
+  margin: 1em auto;
+  text-align: center;
+
+`;
+
+
+export default AppStyled;
---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import AppStyled from './AppStyled';
+
+// == Composant
+const App = () => (
+  <AppStyled>
+    <h1>Test Particeep</h1>
+  </AppStyled>
+);
+
+// == Export
+export default App;
