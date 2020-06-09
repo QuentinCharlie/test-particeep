5daf2eb32358bdf7574f0f12239f871416a272eb
QuentinCharlie <quentin.charlie@orange.fr>
2020-06-10 00:51:45 +0200

    filter menu dynamic with active item css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Menu, Card } from 'semantic-ui-react';
 
@@ -17,30 +17,39 @@ const App = ({
   filterByCategory,
   changeActiveCategory,
 }) => {
+  useEffect(() => {
+    filterByCategory('all');
+    changeActiveCategory('all');
+  }, [filteredMovies.length === 0]);
+  
   const handleFilterClick = (e) => {
     const categoryClicked = e.currentTarget.dataset.category;
     if (activeCategory === categoryClicked) {
       filterByCategory('all');
+      changeActiveCategory('all');
     }
     else {
       filterByCategory(categoryClicked);
       changeActiveCategory(categoryClicked);
     }
   };
+
   return (
     <AppStyled>
       <h1>Test Particeep</h1>
-      <Menu widths={categories.length + 1} className="filter-menu">
+      <Menu inverted pointing widths={categories.length + 1} className="filter-menu">
         <Menu.Item
           key="all"
+          active={activeCategory === 'all'}
           data-category="all"
           onClick={handleFilterClick}
         >
-          Tout
+          All categories
         </Menu.Item>
         {categories.map((category) => (
         <Menu.Item
           key={category}
+          active={activeCategory === category}
           data-category={category}
           onClick={handleFilterClick}
         >
